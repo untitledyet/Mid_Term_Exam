e13ffa45dccc3ee0564b850f366319f32eb140be
Daviti Meskhidze <daviti.meskhidze@gmail.com>
2024-02-10 14:27:59 +0400

    User Validaton

--- Exam/Exam/Program.cs
+++ Exam/Exam/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.IO;
+using System.Text.Json;
 
 namespace Exam
 {
@@ -9,30 +11,39 @@ namespace Exam
             string json =
                 File.ReadAllText(
                     "/Users/meskhdav/Library/CloudStorage/OneDrive-TheStarsGroup/Desktop/Mid_Term_Exam/Exam/Exam/files/Person.json");
-
             Person person = JsonSerializer.Deserialize<Person>(json);
 
-
             Console.Write("შეიყვანეთ ბარათის ნომერი: ");
-            string cardNumber = "";
-            int count = 0;
+            var creditCardNumber = CardNumberInput();
 
-            while (cardNumber.Length < 16)
+            Console.Write("შეიყვანეთ ბარათის მოქმედების ვადა: ");
+            var expDate = ExpiratinDateInput();
+
+            Console.Write("  CVC კოდი: ");
+            var cvc = CvcValidationInput();
+
+
+            
+            if (creditCardNumber == person.CardDetails.CardNumber && expDate == person.CardDetails.ExpirationDate && cvc == person.CardDetails.CVC)
             {
-                ConsoleKeyInfo key = Console.ReadKey(true);
-                if (char.IsDigit(key.KeyChar))
+                Console.Write("PIN : ");
+                var pin = PinValidationInput();
+
+                if (pin==person.PinCode)
                 {
-                    cardNumber += key.KeyChar;
-                    count++;
-                    Console.Write(key.KeyChar);
-                    if (count % 4 == 0 && count != 16)
-                    {
-                        Console.Write("-");
-                    }
+                    
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("არასწორი PIN  ! \u274c ");
                 }
             }
-
-            Console.WriteLine();
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ასეთი ბარათი არ არსებობს ! \u274c ");
+            } 
 
 
             /*
@@ -52,6 +63,220 @@ namespace Exam
                 Console.WriteLine($"  Amount (USD): {transaction.AmountUSD}");
                 Console.WriteLine($"  Amount (EUR): {transaction.AmountEUR}");
             } */
+        }
+
+        static string CardNumberInput()
+        {
+            string cardNumber = "";
+            int count = 0;
+
+            while (cardNumber.Length < 19)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true); //უყურებს კონსოლს
+                if (char.IsDigit(key.KeyChar)) // იღებს მხოლოდ ციფრებს
+                {
+                    cardNumber += key.KeyChar;
+                    count++;
+                    Console.Write(key.KeyChar);
+                    if (count % 4 == 0 && count != 16)
+                    {
+                        Console.Write("-");
+                        cardNumber += "-";
+                    }
+                    else if (key.Key == ConsoleKey.Backspace && cardNumber.Length > 0)
+                    {
+                        if (cardNumber[cardNumber.Length - 1] == '-')
+                        {
+                            cardNumber = cardNumber.Substring(0, cardNumber.Length - 2);
+                            Console.Write("\b \b");
+                            if (count % 4 == 0 && count != 16)
+                            {
+                                count--;
+                                Console.Write("\b \b");
+                            }
+                        }
+                        else
+                        {
+                            cardNumber = cardNumber.Substring(0, cardNumber.Length - 1);
+
+
+                            if (count % 4 != 0 && count != 16)
+                            {
+                                count--;
+                                Console.Write("\b \b");
+                            }
+                        }
+                    }
+                }
+                else if (key.Key == ConsoleKey.Backspace && cardNumber.Length > 0)
+                {
+                    if (cardNumber[cardNumber.Length - 1] == '-')
+                    {
+                        cardNumber = cardNumber.Substring(0, cardNumber.Length - 2);
+                        Console.Write("\b \b");
+                        if (count % 4 == 0 && count != 16)
+                        {
+                            count--;
+                            Console.Write("\b \b");
+                        }
+                    }
+                    else
+                    {
+                        cardNumber = cardNumber.Substring(0, cardNumber.Length - 1);
+
+
+                        if (count % 4 != 0 && count != 16)
+                        {
+                            count--;
+                            Console.Write("\b \b");
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            return cardNumber;
+        }
+
+        static string ExpiratinDateInput()
+        {
+            string expDate = "";
+            int count = 0;
+
+            while (expDate.Length < 5)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true); //უყურებს კონსოლს
+                if (char.IsDigit(key.KeyChar)) // იღებს მხოლოდ ციფრებს
+                {
+                    expDate += key.KeyChar;
+                    count++;
+                    Console.Write(key.KeyChar);
+                    if (count % 2 == 0 && count != 4)
+                    {
+                        Console.Write("/");
+                        expDate += "/";
+                    }
+                    else if (key.Key == ConsoleKey.Backspace && expDate.Length > 0)
+                    {
+                        if (expDate[expDate.Length - 1] == '/')
+                        {
+                            expDate = expDate.Substring(0, expDate.Length - 2);
+                            Console.Write("\b \b");
+                            if (count % 2 == 0 && count != 4)
+                            {
+                                count--;
+                                Console.Write("\b \b");
+                            }
+                        }
+                        else
+                        {
+                            expDate = expDate.Substring(0, expDate.Length - 1);
+
+
+                            if (count % 2 != 0 && count != 4)
+                            {
+                                count--;
+                                Console.Write("\b \b");
+                            }
+                        }
+                    }
+                }
+                else if (key.Key == ConsoleKey.Backspace && expDate.Length > 0)
+                {
+                    if (expDate[expDate.Length - 1] == '/')
+                    {
+                        expDate = expDate.Substring(0, expDate.Length - 2);
+                        Console.Write("\b \b");
+                        if (count % 2 == 0 && count != 4)
+                        {
+                            count--;
+                            Console.Write("\b \b");
+                        }
+                    }
+                    else
+                    {
+                        expDate = expDate.Substring(0, expDate.Length - 1);
+
+
+                        if (count % 2 != 0 && count != 4)
+                        {
+                            count--;
+                            Console.Write("\b \b");
+                        }
+                    }
+                }
+                
+            }
+
+            //Console.WriteLine();
+            
+            return expDate;
+            
+            
+        }
+
+        static string CvcValidationInput()
+        {
+            string cvc = "";
+            int count = 0;
+
+            while (cvc.Length < 3)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (char.IsDigit(key.KeyChar))
+                {
+                    cvc += key.KeyChar;
+                    count++;
+                    Console.Write(key.KeyChar);
+                    
+                }
+                else if (key.Key == ConsoleKey.Backspace && cvc.Length > 0)
+                {
+                    cvc = cvc.Substring(0, cvc.Length - 1);
+
+
+                    if (cvc.Length < 3)
+                    {
+                        count--;
+                        Console.Write("\b \b");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            return cvc;
+        }
+
+        static string PinValidationInput()
+        {
+            string pin = "";
+            int count = 0;
+
+            while (pin.Length < 4)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (char.IsDigit(key.KeyChar))
+                {
+                    pin += key.KeyChar;
+                    count++;
+                    Console.Write("*");
+                    
+                }
+                else if (key.Key == ConsoleKey.Backspace && pin.Length > 0)
+                {
+                    pin = pin.Substring(0, pin.Length - 1);
+
+
+                    if (pin.Length < 4)
+                    {
+                        count--;
+                        Console.Write("\b \b");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            return pin;
         }
     }
 }
--- Exam/Exam/obj/Debug/net8.0/Exam.AssemblyInfo.cs
+++ Exam/Exam/obj/Debug/net8.0/Exam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60c2f864551397bc8a2f6293faa1dab4b04f4109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f0dbb8f18ade19f7c8ec9c23cf48d1b89baba6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
